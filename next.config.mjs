7b39bb9c678c6952c322055558248329452386da
Tristan Michael Lawrence <trislaw@gmail.com>
2024-10-20 21:47:42 -0400

    Revert "silence new sass deprecation warnings"
    This reverts commit 46ea43b251755c7fe9beb91482a13c0720189246.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	sassOptions: {
-		silenceDeprecations: ['legacy-js-api'],
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
